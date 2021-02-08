0bb5b93392d17bd3ee4cbc5524611c50016a9531
Okulmus, Cem <36513234+cem-okulmus@users.noreply.github.com>
2021-02-08 18:04:07 +0100

    Added some comments to structs and methods

--- algorithms/algorithms.go
+++ algorithms/algorithms.go
@@ -2,10 +2,11 @@ package algorithms
 
 import . "github.com/cem-okulmus/BalancedGo/lib"
 
+// Algorithm serves as the common interfacea of all hypergraph decomposition algorithms
 type Algorithm interface {
+	// A Name is useful to identify the individual algorithms in the result
 	Name() string
 	FindDecomp() Decomp
 	FindDecompGraph(G Graph) Decomp
 	SetWidth(K int)
 }
-
