3257c96528a567f0e7077249de56141f8f5e247b
Okulmus, Cem <36513234+cem-okulmus@users.noreply.github.com>
2021-04-08 17:33:30 +0200

    Revert "split decomp for k=ceil(edges/2) implemented"
    This reverts commit 8b7b3474afa9693fa7069f99d30c57659cc2e40d.

--- balanced.go
+++ balanced.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"os/signal"
 	"reflect"
@@ -476,9 +477,11 @@ func main() {
 		} else if *approx > 0 {
 			ch := make(chan int, 1)
 			go func() {
-				firstApprox := algo.SplitDecomp{Graph: parsedGraph}
-				decomp = firstApprox.FindDecomp()
-				k := decomp.CheckWidth()
+				m := parsedGraph.Edges.Len()
+				k := int(math.Ceil(float64(m) / 2))
+				solver.SetWidth(k)
+				decomp = solver.FindDecomp()
+				k = decomp.CheckWidth() // check width again in case it became smaller
 				solved := false
 
 				var newDecomp Decomp
