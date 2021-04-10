73ad67d81863a0c5849dbdb08d647cd9cca47d91
Okulmus, Cem <36513234+cem-okulmus@users.noreply.github.com>
2021-04-10 22:24:09 +0200

    Small refactoring

--- algorithms/splitDecomp.go
+++ algorithms/splitDecomp.go
@@ -4,30 +4,41 @@ import (
 	"github.com/cem-okulmus/BalancedGo/lib"
 )
 
+// SplitDecomp is a special algorihm that only tries to find a decomposition by splitting the
+// hypergraph in two. This is only useful as a first step for the approximation method for finding
+// decomposition.
 type SplitDecomp struct {
 	K     int
 	Graph lib.Graph
 }
 
+// Name returns the name of the algorithm
 func (d *SplitDecomp) Name() string {
 	return "SplitDecomp"
 }
 
+// FindDecomp finds a decomp
 func (d *SplitDecomp) FindDecomp() lib.Decomp {
 	return d.FindDecompGraph(d.Graph)
 }
 
+// SetWidth sets the current width parameter of the algorithm
+func (d *SplitDecomp) SetWidth(K int) {
+	d.K = K
+}
+
+// FindDecompGraph finds a decomp, for an explicit lib.Graph
 func (d *SplitDecomp) FindDecompGraph(G lib.Graph) lib.Decomp {
-	childCover, childBag := split(G.Edges.Slice()[:d.K], len(G.Vertices()))
+	childCover, childBag := split(G.Edges.Slice()[:d.K])
 	child := lib.Node{Bag: childBag, Cover: childCover}
 
-	rootCover, rootBag := split(G.Edges.Slice()[d.K:], len(G.Vertices()))
+	rootCover, rootBag := split(G.Edges.Slice()[d.K:])
 	root := lib.Node{Bag: rootBag, Cover: rootCover, Children: []lib.Node{child}}
 
 	return lib.Decomp{Graph: G, Root: root}
 }
 
-func split(edges []lib.Edge, maxV int) (lib.Edges, []int) {
+func split(edges []lib.Edge) (lib.Edges, []int) {
 	cover := lib.NewEdges(edges)
 	bag := make([]int, 0)
 	bagSet := make(map[int]bool)
@@ -41,8 +52,4 @@ func split(edges []lib.Edge, maxV int) (lib.Edges, []int) {
 		}
 	}
 	return cover, bag
-}
-
-func (d *SplitDecomp) SetWidth(K int) {
-	d.K = K
 }
